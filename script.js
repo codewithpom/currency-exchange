32900138e33eab268a53b667856d8e1ccd46c763
Padmashree Jha <codewithpom@gmail.com>
2021-11-28 20:15:59 +0530

    Done !!!!!!

---
+++ script.js
@@ -0,0 +1,700 @@
+const data = [
+    {
+        "Currency Code": "AED",
+        "Currency Name": "UAE Dirham"
+    },
+    {
+        "Currency Code": "AFN",
+        "Currency Name": "Afghan Afghani"
+    },
+    {
+        "Currency Code": "ALL",
+        "Currency Name": "Albanian Lek"
+    },
+    {
+        "Currency Code": "AMD",
+        "Currency Name": "Armenian Dram"
+    },
+    {
+        "Currency Code": "ANG",
+        "Currency Name": "Netherlands Antillian Guilder"
+    },
+    {
+        "Currency Code": "AOA",
+        "Currency Name": "Angolan Kwanza"
+    },
+    {
+        "Currency Code": "ARS",
+        "Currency Name": "Argentine Peso"
+    },
+    {
+        "Currency Code": "AUD",
+        "Currency Name": "Australian Dollar"
+    },
+    {
+        "Currency Code": "AWG",
+        "Currency Name": "Aruban Florin"
+    },
+    {
+        "Currency Code": "AZN",
+        "Currency Name": "Azerbaijani Manat"
+    },
+    {
+        "Currency Code": "BAM",
+        "Currency Name": "Bosnia and Herzegovina Mark"
+    },
+    {
+        "Currency Code": "BBD",
+        "Currency Name": "Barbados Dollar"
+    },
+    {
+        "Currency Code": "BDT",
+        "Currency Name": "Bangladeshi Taka"
+    },
+    {
+        "Currency Code": "BGN",
+        "Currency Name": "Bulgarian Lev"
+    },
+    {
+        "Currency Code": "BHD",
+        "Currency Name": "Bahraini Dinar"
+    },
+    {
+        "Currency Code": "BIF",
+        "Currency Name": "Burundian Franc"
+    },
+    {
+        "Currency Code": "BMD",
+        "Currency Name": "Bermudian Dollar"
+    },
+    {
+        "Currency Code": "BND",
+        "Currency Name": "Brunei Dollar"
+    },
+    {
+        "Currency Code": "BOB",
+        "Currency Name": "Bolivian Boliviano"
+    },
+    {
+        "Currency Code": "BRL",
+        "Currency Name": "Brazilian Real"
+    },
+    {
+        "Currency Code": "BSD",
+        "Currency Name": "Bahamian Dollar"
+    },
+    {
+        "Currency Code": "BTN",
+        "Currency Name": "Bhutanese Ngultrum"
+    },
+    {
+        "Currency Code": "BWP",
+        "Currency Name": "Botswana Pula"
+    },
+    {
+        "Currency Code": "BYN",
+        "Currency Name": "Belarusian Ruble"
+    },
+    {
+        "Currency Code": "BZD",
+        "Currency Name": "Belize Dollar"
+    },
+    {
+        "Currency Code": "CAD",
+        "Currency Name": "Canadian Dollar"
+    },
+    {
+        "Currency Code": "CDF",
+        "Currency Name": "Congolese Franc"
+    },
+    {
+        "Currency Code": "CHF",
+        "Currency Name": "Swiss Franc"
+    },
+    {
+        "Currency Code": "CLP",
+        "Currency Name": "Chilean Peso"
+    },
+    {
+        "Currency Code": "CNY",
+        "Currency Name": "Chinese Renminbi"
+    },
+    {
+        "Currency Code": "COP",
+        "Currency Name": "Colombian Peso"
+    },
+    {
+        "Currency Code": "CRC",
+        "Currency Name": "Costa Rican Colon"
+    },
+    {
+        "Currency Code": "CUC",
+        "Currency Name": "Cuban Convertible Peso"
+    },
+    {
+        "Currency Code": "CUP",
+        "Currency Name": "Cuban Peso"
+    },
+    {
+        "Currency Code": "CVE",
+        "Currency Name": "Cape Verdean Escudo"
+    },
+    {
+        "Currency Code": "CZK",
+        "Currency Name": "Czech Koruna"
+    },
+    {
+        "Currency Code": "DJF",
+        "Currency Name": "Djiboutian Franc"
+    },
+    {
+        "Currency Code": "DKK",
+        "Currency Name": "Danish Krone"
+    },
+    {
+        "Currency Code": "DOP",
+        "Currency Name": "Dominican Peso"
+    },
+    {
+        "Currency Code": "DZD",
+        "Currency Name": "Algerian Dinar"
+    },
+    {
+        "Currency Code": "EGP",
+        "Currency Name": "Egyptian Pound"
+    },
+    {
+        "Currency Code": "ERN",
+        "Currency Name": "Eritrean Nakfa"
+    },
+    {
+        "Currency Code": "ETB",
+        "Currency Name": "Ethiopian Birr"
+    },
+    {
+        "Currency Code": "EUR",
+        "Currency Name": "Euro"
+    },
+    {
+        "Currency Code": "FJD",
+        "Currency Name": "Fiji Dollar"
+    },
+    {
+        "Currency Code": "FKP",
+        "Currency Name": "Falkland Islands Pound"
+    },
+    {
+        "Currency Code": "FOK",
+        "Currency Name": "Faroese Króna"
+    },
+    {
+        "Currency Code": "GBP",
+        "Currency Name": "Pound Sterling"
+    },
+    {
+        "Currency Code": "GEL",
+        "Currency Name": "Georgian Lari"
+    },
+    {
+        "Currency Code": "GGP",
+        "Currency Name": "Guernsey Pound"
+    },
+    {
+        "Currency Code": "GHS",
+        "Currency Name": "Ghanaian Cedi"
+    },
+    {
+        "Currency Code": "GIP",
+        "Currency Name": "Gibraltar Pound"
+    },
+    {
+        "Currency Code": "GMD",
+        "Currency Name": "Gambian Dalasi"
+    },
+    {
+        "Currency Code": "GNF",
+        "Currency Name": "Guinean Franc"
+    },
+    {
+        "Currency Code": "GTQ",
+        "Currency Name": "Guatemalan Quetzal"
+    },
+    {
+        "Currency Code": "GYD",
+        "Currency Name": "Guyanese Dollar"
+    },
+    {
+        "Currency Code": "HKD",
+        "Currency Name": "Hong Kong Dollar"
+    },
+    {
+        "Currency Code": "HNL",
+        "Currency Name": "Honduran Lempira"
+    },
+    {
+        "Currency Code": "HRK",
+        "Currency Name": "Croatian Kuna"
+    },
+    {
+        "Currency Code": "HTG",
+        "Currency Name": "Haitian Gourde"
+    },
+    {
+        "Currency Code": "HUF",
+        "Currency Name": "Hungarian Forint"
+    },
+    {
+        "Currency Code": "IDR",
+        "Currency Name": "Indonesian Rupiah"
+    },
+    {
+        "Currency Code": "ILS",
+        "Currency Name": "Israeli New Shekel"
+    },
+    {
+        "Currency Code": "IMP",
+        "Currency Name": "Manx Pound"
+    },
+    {
+        "Currency Code": "INR",
+        "Currency Name": "Indian Rupee"
+    },
+    {
+        "Currency Code": "IQD",
+        "Currency Name": "Iraqi Dinar"
+    },
+    {
+        "Currency Code": "IRR",
+        "Currency Name": "Iranian Rial"
+    },
+    {
+        "Currency Code": "ISK",
+        "Currency Name": "Icelandic Króna"
+    },
+    {
+        "Currency Code": "JMD",
+        "Currency Name": "Jamaican Dollar"
+    },
+    {
+        "Currency Code": "JOD",
+        "Currency Name": "Jordanian Dinar"
+    },
+    {
+        "Currency Code": "JPY",
+        "Currency Name": "Japanese Yen"
+    },
+    {
+        "Currency Code": "KES",
+        "Currency Name": "Kenyan Shilling"
+    },
+    {
+        "Currency Code": "KGS",
+        "Currency Name": "Kyrgyzstani Som"
+    },
+    {
+        "Currency Code": "KHR",
+        "Currency Name": "Cambodian Riel"
+    },
+    {
+        "Currency Code": "KID",
+        "Currency Name": "Kiribati Dollar"
+    },
+    {
+        "Currency Code": "KMF",
+        "Currency Name": "Comorian Franc"
+    },
+    {
+        "Currency Code": "KRW",
+        "Currency Name": "South Korean Won"
+    },
+    {
+        "Currency Code": "KWD",
+        "Currency Name": "Kuwaiti Dinar"
+    },
+    {
+        "Currency Code": "KYD",
+        "Currency Name": "Cayman Islands Dollar"
+    },
+    {
+        "Currency Code": "KZT",
+        "Currency Name": "Kazakhstani Tenge"
+    },
+    {
+        "Currency Code": "LAK",
+        "Currency Name": "Lao Kip"
+    },
+    {
+        "Currency Code": "LBP",
+        "Currency Name": "Lebanese Pound"
+    },
+    {
+        "Currency Code": "LKR",
+        "Currency Name": "Sri Lanka Rupee"
+    },
+    {
+        "Currency Code": "LRD",
+        "Currency Name": "Liberian Dollar"
+    },
+    {
+        "Currency Code": "LSL",
+        "Currency Name": "Lesotho Loti"
+    },
+    {
+        "Currency Code": "LYD",
+        "Currency Name": "Libyan Dinar"
+    },
+    {
+        "Currency Code": "MAD",
+        "Currency Name": "Moroccan Dirham"
+    },
+    {
+        "Currency Code": "MDL",
+        "Currency Name": "Moldovan Leu"
+    },
+    {
+        "Currency Code": "MGA",
+        "Currency Name": "Malagasy Ariary"
+    },
+    {
+        "Currency Code": "MKD",
+        "Currency Name": "Macedonian Denar"
+    },
+    {
+        "Currency Code": "MMK",
+        "Currency Name": "Burmese Kyat"
+    },
+    {
+        "Currency Code": "MNT",
+        "Currency Name": "Mongolian Tögrög"
+    },
+    {
+        "Currency Code": "MOP",
+        "Currency Name": "Macanese Pataca"
+    },
+    {
+        "Currency Code": "MRU",
+        "Currency Name": "Mauritanian Ouguiya"
+    },
+    {
+        "Currency Code": "MUR",
+        "Currency Name": "Mauritian Rupee"
+    },
+    {
+        "Currency Code": "MVR",
+        "Currency Name": "Maldivian Rufiyaa"
+    },
+    {
+        "Currency Code": "MWK",
+        "Currency Name": "Malawian Kwacha"
+    },
+    {
+        "Currency Code": "MXN",
+        "Currency Name": "Mexican Peso"
+    },
+    {
+        "Currency Code": "MYR",
+        "Currency Name": "Malaysian Ringgit"
+    },
+    {
+        "Currency Code": "MZN",
+        "Currency Name": "Mozambican Metical"
+    },
+    {
+        "Currency Code": "NAD",
+        "Currency Name": "Namibian Dollar"
+    },
+    {
+        "Currency Code": "NGN",
+        "Currency Name": "Nigerian Naira"
+    },
+    {
+        "Currency Code": "NIO",
+        "Currency Name": "Nicaraguan Córdoba"
+    },
+    {
+        "Currency Code": "NOK",
+        "Currency Name": "Norwegian Krone"
+    },
+    {
+        "Currency Code": "NPR",
+        "Currency Name": "Nepalese Rupee"
+    },
+    {
+        "Currency Code": "NZD",
+        "Currency Name": "New Zealand Dollar"
+    },
+    {
+        "Currency Code": "OMR",
+        "Currency Name": "Omani Rial"
+    },
+    {
+        "Currency Code": "PAB",
+        "Currency Name": "Panamanian Balboa"
+    },
+    {
+        "Currency Code": "PEN",
+        "Currency Name": "Peruvian Sol"
+    },
+    {
+        "Currency Code": "PGK",
+        "Currency Name": "Papua New Guinean Kina"
+    },
+    {
+        "Currency Code": "PHP",
+        "Currency Name": "Philippine Peso"
+    },
+    {
+        "Currency Code": "PKR",
+        "Currency Name": "Pakistani Rupee"
+    },
+    {
+        "Currency Code": "PLN",
+        "Currency Name": "Polish Złoty"
+    },
+    {
+        "Currency Code": "PYG",
+        "Currency Name": "Paraguayan Guaraní"
+    },
+    {
+        "Currency Code": "QAR",
+        "Currency Name": "Qatari Riyal"
+    },
+    {
+        "Currency Code": "RON",
+        "Currency Name": "Romanian Leu"
+    },
+    {
+        "Currency Code": "RSD",
+        "Currency Name": "Serbian Dinar"
+    },
+    {
+        "Currency Code": "RUB",
+        "Currency Name": "Russian Ruble"
+    },
+    {
+        "Currency Code": "RWF",
+        "Currency Name": "Rwandan Franc"
+    },
+    {
+        "Currency Code": "SAR",
+        "Currency Name": "Saudi Riyal"
+    },
+    {
+        "Currency Code": "SBD",
+        "Currency Name": "Solomon Islands Dollar"
+    },
+    {
+        "Currency Code": "SCR",
+        "Currency Name": "Seychellois Rupee"
+    },
+    {
+        "Currency Code": "SDG",
+        "Currency Name": "Sudanese Pound"
+    },
+    {
+        "Currency Code": "SEK",
+        "Currency Name": "Swedish Krona"
+    },
+    {
+        "Currency Code": "SGD",
+        "Currency Name": "Singapore Dollar"
+    },
+    {
+        "Currency Code": "SHP",
+        "Currency Name": "Saint Helena Pound"
+    },
+    {
+        "Currency Code": "SLL",
+        "Currency Name": "Sierra Leonean Leone"
+    },
+    {
+        "Currency Code": "SOS",
+        "Currency Name": "Somali Shilling"
+    },
+    {
+        "Currency Code": "SRD",
+        "Currency Name": "Surinamese Dollar"
+    },
+    {
+        "Currency Code": "SSP",
+        "Currency Name": "South Sudanese Pound"
+    },
+    {
+        "Currency Code": "STN",
+        "Currency Name": "São Tomé and Príncipe Dobra"
+    },
+    {
+        "Currency Code": "SYP",
+        "Currency Name": "Syrian Pound"
+    },
+    {
+        "Currency Code": "SZL",
+        "Currency Name": "Eswatini Lilangeni"
+    },
+    {
+        "Currency Code": "THB",
+        "Currency Name": "Thai Baht"
+    },
+    {
+        "Currency Code": "TJS",
+        "Currency Name": "Tajikistani Somoni"
+    },
+    {
+        "Currency Code": "TMT",
+        "Currency Name": "Turkmenistan Manat"
+    },
+    {
+        "Currency Code": "TND",
+        "Currency Name": "Tunisian Dinar"
+    },
+    {
+        "Currency Code": "TOP",
+        "Currency Name": "Tongan Paʻanga"
+    },
+    {
+        "Currency Code": "TRY",
+        "Currency Name": "Turkish Lira"
+    },
+    {
+        "Currency Code": "TTD",
+        "Currency Name": "Trinidad and Tobago Dollar"
+    },
+    {
+        "Currency Code": "TVD",
+        "Currency Name": "Tuvaluan Dollar"
+    },
+    {
+        "Currency Code": "TWD",
+        "Currency Name": "New Taiwan Dollar"
+    },
+    {
+        "Currency Code": "TZS",
+        "Currency Name": "Tanzanian Shilling"
+    },
+    {
+        "Currency Code": "UAH",
+        "Currency Name": "Ukrainian Hryvnia"
+    },
+    {
+        "Currency Code": "UGX",
+        "Currency Name": "Ugandan Shilling"
+    },
+    {
+        "Currency Code": "USD",
+        "Currency Name": "United States Dollar"
+    },
+    {
+        "Currency Code": "UYU",
+        "Currency Name": "Uruguayan Peso"
+    },
+    {
+        "Currency Code": "UZS",
+        "Currency Name": "Uzbekistani So'm"
+    },
+    {
+        "Currency Code": "VES",
+        "Currency Name": "Venezuelan Bolívar Soberano"
+    },
+    {
+        "Currency Code": "VND",
+        "Currency Name": "Vietnamese Đồng"
+    },
+    {
+        "Currency Code": "VUV",
+        "Currency Name": "Vanuatu Vatu"
+    },
+    {
+        "Currency Code": "WST",
+        "Currency Name": "Samoan Tālā"
+    },
+    {
+        "Currency Code": "XAF",
+        "Currency Name": "Central African CFA Franc"
+    },
+    {
+        "Currency Code": "XCD",
+        "Currency Name": "East Caribbean Dollar"
+    },
+    {
+        "Currency Code": "XDR",
+        "Currency Name": "Special Drawing Rights"
+    },
+    {
+        "Currency Code": "XOF",
+        "Currency Name": "West African CFA franc"
+    },
+    {
+        "Currency Code": "XPF",
+        "Currency Name": "CFP Franc"
+    },
+    {
+        "Currency Code": "YER",
+        "Currency Name": "Yemeni Rial"
+    },
+    {
+        "Currency Code": "ZAR",
+        "Currency Name": "South African Rand"
+    },
+    {
+        "Currency Code": "ZMW",
+        "Currency Name": "Zambian Kwacha"
+    }
+]
+
+const input_select = document.getElementById("from")
+const output_select = document.getElementById("to")
+const spinner = document.getElementById("spinner")
+const input_input_tag = document.getElementById("amount");
+const output_input_tag = document.getElementById("result");
+
+for (const element of data) {
+    const HTMLElement =
+        `
+        <option value="${element["Currency Code"]}">
+            ${element["Currency Name"]}
+        </option>
+    `
+
+    input_select.innerHTML += HTMLElement;
+    output_select.innerHTML += HTMLElement;
+
+}
+
+document.body.onload = refresh_data();
+
+function update_out_values() {
+    const rates = window.currency_rate;
+    const out_curreny = output_select.value;
+    const in_value = Number(input_input_tag.value);
+    const result = rates[out_curreny] * in_value;
+    output_input_tag.value = result;
+}
+
+async function refresh_data() {
+    const input_value = input_select.value;
+    spinner.className = "text-center";
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/1ffd810222f23eafe4b5b650/latest/${input_value}`)
+    const data = await response.json()
+    window.currency_rate = data.conversion_rates;
+    spinner.className = "text-center invisible";
+    update_out_values();
+}
+
+
+// add event listener when the input select is changed
+input_select.addEventListener("change", async function () {
+    refresh_data();
+
+})
+
+// add event listener when the output select is changed
+output_select.addEventListener("change", async function () {
+    update_out_values();
+})
+
+
+// add event listener when the input input tag is changed
+input_input_tag.addEventListener("change", async function () {
+    update_out_values();
+})
+
